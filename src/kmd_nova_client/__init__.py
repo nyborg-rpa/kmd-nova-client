0b1279258446ac897bc8b0ef2e55856ebdd0f144
Martin Androvich <martinandrovich@gmail.com>
2025-05-27 12:45:13 +0200

    add KMDNovaClient

--- src/kmd_nova_client/__init__.py
+++ src/kmd_nova_client/__init__.py
@@ -1,2 +1,5 @@
-def hello() -> str:
-    return "Hello from kmd-nova-client!"
+__all__ = [
+    "KMDNovaClient",
+]
+
+from kmd_nova_client._client import KMDNovaClient
---
+++ src/kmd_nova_client/_client.py
@@ -0,0 +1,3 @@
+class KMDNovaClient:
+
+    BASE_URL = "https://cap-awswlbs-wm3q2021.kmd.dk/KMDNovaESDH"
